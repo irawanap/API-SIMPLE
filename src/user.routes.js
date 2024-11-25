fe508556d3ba9e6fffe8c59c190035fecde53e8e
irawanap <irawanajhi22@student.ub.ac.id>
2024-11-26 00:19:05 +0700

    m

--- src/user.routes.js
+++ src/user.routes.js
@@ -1,102 +1,28 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-import userService from './services/user.service';
+import { expressYupMiddleware } from 'express-yup-middleware';
+
+import userController from './controllers/user.controller';
+import {getUser, addUser, updateUser, removeUser} from './user.schemas';
 
 const router = express.Router();
 
-const STATUS = {
-    success: 'OK',
-    failure: 'NO'
-};
+router.get('/all', userController.getAllUsers);
 
-router.get('/all', (req, res) => {
+router.get('/:id', expressYupMiddleware({schemaValidator: getUser}),
+    userController.getUser
+);
 
-    const users = userService.getAllUsers();
+router.post('/', expressYupMiddleware({schemaValidator: addUser}),
+    userController.addUser
+);
 
-    if (users.length) {
-        return res.status(StatusCodes.OK).send(users);
-    }
+router.put('/:id', expressYupMiddleware({schemaValidator: updateUser}),
+    userController.updateUser
+);
 
-    return res.status(StatusCodes.NOT_FOUND).send(
-        {
-            status: STATUS.failure,
-            message: 'No users found',
-        }
-    );
-});
-
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const user = userService.getUser(id);
-
-    if (user) {
-        return res.status(StatusCodes.OK).send(
-            {
-                status: STATUS.success,
-                data: user,
-            }
-        );
-    } 
-    
-    return res.status(StatusCodes.NOT_FOUND).send(
-        {
-            status: STATUS.failure,
-            message: `User ${id} is not found.`,
-        }
-    )
-});
-
-router.post('/', (req,res) => {
-    const { body: user } = req;
-
-    const addedUser = userService.addUser(user);
-
-    return res.status(StatusCodes.CREATED).send( {
-        status: STATUS.success,
-        user: addedUser,
-    });
-    
-});
-
-router.put('/:id', (req,res) => {
-    const { body: user } = req;
-
-    const id = parseInt(req.params.id, 10)
-
-    const updatedUser = userService.updateUser(id, user);
-
-    if (updatedUser) {
-    return res.status(StatusCodes.OK).send( {
-        status: STATUS.success,
-        data: updatedUser,
-    });
-    } else {
-        return res.status(StatusCodes.NOT_FOUND).send( {
-            status: STATUS.failure,
-            message: `User ${id} is not found.`,
-        });
-    }
-});
-
-router.delete('/:id', (req, res) => {
-    const { params } = req;
-
-    const id = parseInt(params.id);
-    const user = userService.getUser(id);
-    if (user) {
-        userService.removeUser(id);
-
-        return res.status(StatusCodes.OK).send( {
-            status: STATUS.success,
-            message: `User ${id} has been deleted.`,
-        });
-    } else {
-        return res.status(StatusCodes.OK).send({
-            status: STATUS.failure,
-            message: `User ${id} hasn't been found.`,
-        }
-    )};
-    
-})
+router.delete('/:id', expressYupMiddleware({schemaValidator: removeUser}),
+    userController.removeUser
+);
 
 export default router;
